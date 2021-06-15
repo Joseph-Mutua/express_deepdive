3f98943ab9b9319242a0229c4f8fde5ad89c7407
Joseph Mutua <mutuaj793@gmail.com>
2021-06-15 16:55:21 +0300

    Serving up routes and static files in plain node

--- nodeServer.js
+++ nodeServer.js
@@ -1,12 +1,26 @@
 const http = require("http");
+const fs = require("fs");
 
 //Setup server
 const server = http.createServer((req, res) => {
   //   console.log(req);
 
-  res.writeHead(200, { "content-type": "text/html" });
-  res.write("<h1>Hello  world!</h1>")
-  res.end()
+  if (req.url === "/") {
+    res.writeHead(200, { "content-type": "text/html" });
+    // res.write("");
+    const homePageHTML = fs.readFileSync("node.html");
+    res.write(homePageHTML);
+    res.end();
+  } else if ((req.url = "/styles.css")) {
+    res.writeHead(200, { "content-type": "text/css" });
+    const css = fs.readFileSync("styles.css");
+    res.write(css);
+    res.end();
+  } else {
+    res.writeHead(404, { "content-type": "text/html" });
+    res.write("<h1>Sorry, this isnt the page you're looking for</h1>");
+    res.end();
+  }
 });
 
 server.listen(3000);
