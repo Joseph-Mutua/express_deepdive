6d995a7b70c714e229492afa5e3414c60a23570d
Joseph Mutua <mutuaj793@gmail.com>
2021-06-16 20:02:25 +0300

    Set up basic express server

---
+++ expressServer.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.all("*", (req, res) => {
+  res.send("<h1>Express Homepage!</>");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
