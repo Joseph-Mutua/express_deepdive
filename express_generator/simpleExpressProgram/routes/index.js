464081c5533a39cd64a3170c4d890fd7f9140da8
Joseph Mutua <mutuaj793@gmail.com>
2021-06-19 17:40:58 +0300

    Playing around with http headers

--- express_generator/simpleExpressProgram/routes/index.js
+++ express_generator/simpleExpressProgram/routes/index.js
@@ -1,9 +1,19 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  
+  //Modifying HTTP Headers
+  const date = new Date(1998, 2, 21);
+  res.set("Date", date);
+
+  //Accept no cached data
+  //Only good for development
+  res.set("Cache-Control", "no-store");
+  res.set("Content-Type", "text/plain");
+
+  res.render("index", { title: "Express" });
 });
 
 module.exports = router;
